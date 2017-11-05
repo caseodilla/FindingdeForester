bcf26beb1511976edc0fecb3845d45fab7b5a630
Your Name <jgreenwell@elon.edu>
2017-11-05 01:47:26 -0500

    fixing player rendering

--- FindingDeForester/src/main/java/edu/elon/ai/proxy/ClientProxy.java
+++ FindingDeForester/src/main/java/edu/elon/ai/proxy/ClientProxy.java
@@ -1,15 +1,22 @@
 package edu.elon.ai.proxy;
 
+import edu.elon.ai.entities.EntityAI;
 import edu.elon.ai.items.ModItems;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.model.ModelPlayer;
+import net.minecraft.client.renderer.entity.RenderLiving;
+import net.minecraft.client.renderer.entity.RenderPlayer;
+import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 public class ClientProxy extends CommonProxy{
    
+	@SuppressWarnings("deprecation")
 	@Override
     public void preInit(FMLPreInitializationEvent e) {
+		RenderingRegistry.registerEntityRenderingHandler(EntityAI.class, new RenderPlayer(Minecraft.getMinecraft().getRenderManager()));
         super.preInit(e);
         
     }
@@ -17,7 +24,6 @@ public class ClientProxy extends CommonProxy{
     @Override
     public void init(FMLInitializationEvent e) {
         super.init(e);
-        //ModItems.setupItems(Minecraft.getMinecraft().getRenderItem().getItemModelMesher());
     }
 
     @Override
