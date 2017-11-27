8da7c37fafd0dd82c8e5c082a5a185a5db701cd1
caseodilla <caseodilla@gmail.com>
2017-11-26 21:58:42 -0500

    added TravelingSalesman population

--- FindingDeForester/src/main/java/edu/elon/ai/problems/TravelingSalesman.java
+++ FindingDeForester/src/main/java/edu/elon/ai/problems/TravelingSalesman.java
@@ -3,16 +3,59 @@ package edu.elon.ai.problems;
 import java.util.ArrayList;
 
 import edu.elon.ai.datastructures.Location3D;
+import edu.elon.ai.datastructures.TravelPath;
 
 //This will be both GA and the population class from the website, you can make them separate if you want
 //it may be easier to keep the functionality all in the same class tho
 //http://www.theprojectspot.com/tutorial-post/applying-a-genetic-algorithm-to-the-travelling-salesman-problem/5
 public class TravelingSalesman {
-	ArrayList<Location3D> locations;
-	public TravelingSalesman(ArrayList<Location3D> locations){
-		this.locations = locations;
+
+/*11/26 CH: Does this need to be here? It looks similar to the code in TravelPath 	
+*	ArrayList<Location3D> locations;
+*	public TravelingSalesman(ArrayList<Location3D> locations){
+*		this.locations = locations;
+*	}
+*/	
+	
+	// holds the population of different generated paths
+	TravelPath[] paths;
+	
+	// create a population
+	public TravelingSalesman(int popSize, boolean initialise) {
+		paths = new TravelPath[popSize];
+		// in case we need to init a population of paths
+		if (initialise) {
+			for (int i=0; i<populationSize(); i++) {
+				TravelPath newPath = new TravelPath();
+				newPath.generateIndividual();
+				savePath(i,newPath);
+			}
+		}
 	}
 	
+	// save the path
+	public void savePath(int i, TravelPath path) {
+		paths[i] = path;
+	}
 	
+	// get the path from the population
+	public TravelPath getPath(int i) {
+		return paths[i];
+	}
 	
+	// get the best path in the population
+	public TravelPath getFittest() {
+		TravelPath fittest = paths[0];
+		// loop through all paths, find the fittest
+		for (int i=1; i<populationSize(); i++) {
+			if (fittest.getFitness() <= getPath(i).getFitness()) {
+				fittest = getPath(i);
+			}
+		}
+		return fittest;
+	}
+	
+	public int populationSize() {
+		return paths.length;
+	}
 }
