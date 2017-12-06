bad6f14cbeaaae3730af0fee3fda08c81f17d83f
Your Name <jgreenwell@elon.edu>
2017-12-06 09:29:45 -0500

    added a few more tests

--- FindingDeForester/src/main/java/edu/elon/ai/tester/TravelingSalesmanTest.java
+++ FindingDeForester/src/main/java/edu/elon/ai/tester/TravelingSalesmanTest.java
@@ -44,7 +44,7 @@ public class TravelingSalesmanTest {
 	}
 	
 	@Test
-	public void alwaysImprovingTest() {
+	public void alwaysImprovingTestX() {
 		ArrayList<Location3D> locations = new ArrayList<Location3D>();
 		Location3D playerLocation = new Location3D(0,0,0);
 		//locations.add(playerLocation);
@@ -79,11 +79,11 @@ public class TravelingSalesmanTest {
 		Population population = new Population(playerLocation, locations);
 		//best distance
 		double bestSoFar = population.getFittest().getDistance();
-		for (int i=0; i<11000; i++) {
-			//makes sure it's improving every 1000 generations
+		for (int i=0; i<12000; i++) {
+			//makes sure it's improving every 100 generations
 			if(i%100 == 0) {
 				double current = population.getFittest().getDistance();
-				System.out.println("Current Best Distance: " + current);
+				//System.out.println("Current Best Distance: " + current);
 				if(bestSoFar < current) {
 					fail("The path did not stay the same or improve");
 				}
@@ -93,11 +93,146 @@ public class TravelingSalesmanTest {
 			}
 			population = GeneticAlgorithm.evolvePopulation(playerLocation, population);
 		}
-		System.out.println("||Test Results||");
+		System.out.println("||Test Results - Always Improving Test X||");
+		System.out.println("Final distance: " + population.getFittest().getDistance());
+        System.out.println("Solution:");
+        System.out.println(population.getFittest());
+	}
+	
+	@Test
+	public void alwaysImprovingTestY() {
+		ArrayList<Location3D> locations = new ArrayList<Location3D>();
+		Location3D playerLocation = new Location3D(0,0,0);
+		//locations.add(playerLocation);
+		locations.add(new Location3D(0,28,0));
+		locations.add(new Location3D(0,27,0));
+		locations.add(new Location3D(0,26,0));
+		locations.add(new Location3D(0,25,0));
+		locations.add(new Location3D(0,24,0));
+		locations.add(new Location3D(0,23,0));
+		locations.add(new Location3D(0,22,0));
+		locations.add(new Location3D(0,21,0));
+		locations.add(new Location3D(0,20,0));
+		locations.add(new Location3D(0,19,0));
+		locations.add(new Location3D(0,18,0));
+		locations.add(new Location3D(0,17,0));
+		locations.add(new Location3D(0,16,0));
+		locations.add(new Location3D(0,15,0));
+		locations.add(new Location3D(0,14,0));
+		locations.add(new Location3D(0,13,0));
+		locations.add(new Location3D(0,12,0));
+		locations.add(new Location3D(0,11,0));
+		locations.add(new Location3D(0,10,0));
+		locations.add(new Location3D(0,9,0));
+		locations.add(new Location3D(0,8,0));
+		locations.add(new Location3D(0,7,0));
+		locations.add(new Location3D(0,6,0));
+		locations.add(new Location3D(0,5,0));
+		locations.add(new Location3D(0,4,0));
+		locations.add(new Location3D(0,3,0));
+		locations.add(new Location3D(0,2,0));
+		locations.add(new Location3D(0,1,0));
+		Population population = new Population(playerLocation, locations);
+		//best distance
+		double bestSoFar = population.getFittest().getDistance();
+		for (int i=0; i<12000; i++) {
+			//makes sure it's improving every 100 generations
+			if(i%100 == 0) {
+				double current = population.getFittest().getDistance();
+				//System.out.println("Current Best Distance: " + current);
+				if(bestSoFar < current) {
+					fail("The path did not stay the same or improve");
+				}
+				else {
+					bestSoFar = population.getFittest().getDistance();
+				}
+			}
+			population = GeneticAlgorithm.evolvePopulation(playerLocation, population);
+		}
+		System.out.println("||Test Results - Always Improving Test Y||");
+		System.out.println("Final distance: " + population.getFittest().getDistance());
+        System.out.println("Solution:");
+        System.out.println(population.getFittest());
+	}
+	
+	@Test
+	public void alwaysImprovingTestZ() {
+		ArrayList<Location3D> locations = new ArrayList<Location3D>();
+		Location3D playerLocation = new Location3D(0,0,0);
+		//locations.add(playerLocation);
+		locations.add(new Location3D(0,0,28));
+		locations.add(new Location3D(0,0,27));
+		locations.add(new Location3D(0,0,26));
+		locations.add(new Location3D(0,0,25));
+		locations.add(new Location3D(0,0,24));
+		locations.add(new Location3D(0,0,23));
+		locations.add(new Location3D(0,0,22));
+		locations.add(new Location3D(0,0,21));
+		locations.add(new Location3D(0,0,20));
+		locations.add(new Location3D(0,0,19));
+		locations.add(new Location3D(0,0,18));
+		locations.add(new Location3D(0,0,17));
+		locations.add(new Location3D(0,0,16));
+		locations.add(new Location3D(0,0,15));
+		locations.add(new Location3D(0,0,14));
+		locations.add(new Location3D(0,0,13));
+		locations.add(new Location3D(0,0,12));
+		locations.add(new Location3D(0,0,11));
+		locations.add(new Location3D(0,0,10));
+		locations.add(new Location3D(0,0,9));
+		locations.add(new Location3D(0,0,8));
+		locations.add(new Location3D(0,0,7));
+		locations.add(new Location3D(0,0,6));
+		locations.add(new Location3D(0,0,5));
+		locations.add(new Location3D(0,0,4));
+		locations.add(new Location3D(0,0,3));
+		locations.add(new Location3D(0,0,2));
+		locations.add(new Location3D(0,0,1));
+		Population population = new Population(playerLocation, locations);
+		//best distance
+		double bestSoFar = population.getFittest().getDistance();
+		for (int i=0; i<12000; i++) {
+			//makes sure it's improving every 100 generations
+			if(i%100 == 0) {
+				double current = population.getFittest().getDistance();
+				//System.out.println("Current Best Distance: " + current);
+				if(bestSoFar < current) {
+					fail("The path did not stay the same or improve");
+				}
+				else {
+					bestSoFar = population.getFittest().getDistance();
+				}
+			}
+			population = GeneticAlgorithm.evolvePopulation(playerLocation, population);
+		}
+		System.out.println("||Test Results - Always Improving Test Z||");
+		System.out.println("Final distance: " + population.getFittest().getDistance());
+        System.out.println("Solution:");
+        System.out.println(population.getFittest());
+	}
+	
+	@Test
+	public void effectivenessTest() {
+		ArrayList<Location3D> locations = new ArrayList<Location3D>();
+		Location3D playerLocation = new Location3D(0,0,0);
+		//locations.add(playerLocation);
+		locations.add(new Location3D(1,1,1));
+		locations.add(new Location3D(2,2,2));
+		locations.add(new Location3D(3,3,3));
+		locations.add(new Location3D(4,4,4));
+		Population population = new Population(playerLocation,locations);
+		for (int i=0; i<10000; i++) {
+			population = GeneticAlgorithm.evolvePopulation(playerLocation,population);
+		}
+		System.out.println("||Test Results - Effectiveness Test||");
 		System.out.println("Final distance: " + population.getFittest().getDistance());
         System.out.println("Solution:");
         System.out.println(population.getFittest());
 		
+		//fail("Not yet implemented");
 	}
-
+	
+	
+	
+	
 }
